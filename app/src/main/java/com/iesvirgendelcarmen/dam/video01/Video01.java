d9fbad97cb776486a99a8e55514b54659f41cb2e
jaestrella <jaestrella1991@gmail.com>
2018-02-07 13:47:52 +0100

    Initial commit

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/video01/Video01.java
@@ -0,0 +1,13 @@
+package com.iesvirgendelcarmen.dam.video01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Video01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video01);
+    }
+}
